3573163788476f0ffa0006e85c1d5ff65316e910
Fernando Almeida <almeidascvn@gmail.com>
2018-01-07 12:22:16 -0200

    Chapters 1 and 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-examples
+
+Python exmaples from Head First Book and other code practices
---
+++ nester-ch1_2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name         = 'nester',
+    version      = '1.0.0',
+    py_modules   = ['nester'],
+    author       = 'falmeidapy',
+    author_email = 'almeidascvn@gmail.com',
+    url          = 'fernandoalmeida.esy.es',
+    description  = 'Meu primeiro módulo Python - @almeidajava'
+)
+
